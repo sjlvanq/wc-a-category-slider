46fa1022e891523655ef1aec5b8e41290914b012
sjlvanq <correo@lode.uno>
2024-03-24 01:38:44 -0300

    Update

--- wc-a-category-slider.php
+++ wc-a-category-slider.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: A Woocommerce categories slider
 Description: A Woocommerce categories slider
-Version: dev-0.1
+Version: dev-0.2
 Author: Silvano Emanuel Roqués
 Author URI: http://lode.uno/tejne
 */
@@ -24,24 +24,28 @@ add_action( 'wp_enqueue_scripts', 'wpwcacs_enqueue_script' );
 function wpwcacs_shortcode( $atts = [], $content = null) {
 	$categories = get_categories(
 		array(
-            'hide_empty' =>  0,
+            'hide_empty' =>  1,
             'exclude'  =>  1,
             'taxonomy'   =>  'product_cat' 
 		));
 	$slider_menu="";
 	$slider_content="";
-	foreach( $categories as $index=>$category ) {
-		//var_dump($category);
-		if($category->count > 0){
-			$slider_menu .= '<button style="margin:20px 10px;padding:20px;width:10em;" onclick="currentDiv('.($index).')">'.$category->cat_name.'</button>';
-			$block_content = '<!-- wp:woocommerce/product-category {"categories":['.$category->cat_ID.'],"stockStatus":["","instock","onbackorder"]} /-->';
-			$slider_content .= '<div class="catslide"';
-			if($index>0){$slider_content .= ' style="display:none" ';}
+    // Slide animation
+	$slider_content="<style>.animate-right{position:relative;animation:animateright 0.4s}@keyframes animateright{from{right:-300px;opacity:0} to{right:0;opacity:1}}</style>";  
+	$index=0;
+	foreach( $categories as $category ) {
+		if($category->count > 0 && count(wc_get_products ( array('category' => array( $category->name ), 'stock_status' => 'instock') ))){
+			$slider_menu .= '<button style="margin:20px 10px;padding:20px;width:10em;" onclick="wpwcacs_currentDiv('.($index).')">'.$category->cat_name.'</button>';
+			$block_content = '<!-- wp:woocommerce/product-category {"contentVisibility":{"image":true,"title":true,"price":true,"rating":false,"button":true},"categories":['.$category->cat_ID.'],"stockStatus":["","instock","onbackorder"],"columns":4} /-->';
+			$slider_content .= '<div class="catslide animate-right"';
+			$slider_content .= ' style="display:none" ';
 			$slider_content .= '>'.do_blocks($block_content).'</div>';
+			$index++;
 		}
 	}
 	return '
-		<div style="width:100%;">
+		
+		<div id="catslide_container" style="width:100%;">
 			<div style="display: flex; flex-flow:row wrap; margin-bottom:10px">'.$slider_menu.'</div>
 			<div>'.$slider_content.'</div>
 		</div>';
--- wpwcacs-slider.js
+++ wpwcacs-slider.js
@@ -1,24 +1,24 @@
 //https://www.w3schools.com/w3css/w3css_slideshow.asp
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function wpwcacs_currentDiv(n) {
+  wpwcacs_showDivs(slideIndex = n);
+  //wpwcacs_scrollToSlider();
 }
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
+function wpwcacs_showDivs(n) {
   var x = document.getElementsByClassName("catslide");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
+  if (x.length === 0) {return}
+  for (let i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
-  console.log("wpwcacs-slider");
+  x[slideIndex].classList.add("animate-right");
+  x[slideIndex].style.display = "block";
+  //console.log("wpwcacs-slider");
 }
+
+function wpwcacs_scrollToSlider(){
+
+}
+
+var slideIndex = 0;
+window.addEventListener("load", function(){wpwcacs_currentDiv(0)});
